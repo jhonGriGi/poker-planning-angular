5211f2360cc56b9659cab857f1c8a4cd93c1a199
Darwin Grisales <darwingrisalesg@gmail.com>
2024-03-05 23:09:11 -0500

    test: karma execution on windows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'poker-planning-angular';
+export class AppComponent implements OnInit {
+  @Input() title = 'poker-planning-angular';
+  @Output() valueChange = new EventEmitter<string>();
+  userForm: any;
+
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      age: ['', [Validators.required, Validators.min(20), Validators.max(50)]],
+      gender: ['', Validators.required],
+    });
+  }
+
+  submitForm(): void {
+    if (this.userForm?.valid) {
+      console.log('Form data:', this.userForm.value);
+    }
+
+    console.log(this.userForm.value);
+  }
 }
